2495fe32354bba22dd3fb3979e714653aca0d924
arun.ragav.s.138@kalvium.community <arun.ragav.s.138@kalvium.community>
2026-08-03 08:48:47 +0530

    Create calculator project

---
+++ app.js
@@ -0,0 +1,47 @@
+let display = document.getElementById("display");
+
+
+// Add number or operator
+function appendValue(value) {
+
+    if (display.value === "0") {
+        display.value = value;
+    } 
+    else {
+        display.value += value;
+    }
+}
+
+
+// Clear everything
+function clearDisplay() {
+
+    display.value = "0";
+}
+
+
+// Delete last character
+function deleteNumber() {
+
+    display.value = display.value.slice(0, -1);
+
+    if (display.value === "") {
+        display.value = "0";
+    }
+}
+
+
+// Calculate answer
+function calculate() {
+
+    try {
+
+        display.value = eval(display.value);
+
+    } 
+    catch {
+
+        display.value = "Error";
+
+    }
+}
